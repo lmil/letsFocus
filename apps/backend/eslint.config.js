c213b8d186598f68cf56f7a2042aff80955fafa0
Luthfi Milham <luthfim.ilham@gmail.com>
2026-02-21 11:42:13 +0700

    chore: configure Prettier for frontend and backend

--- apps/backend/eslint.config.js
+++ apps/backend/eslint.config.js
@@ -2,12 +2,13 @@ import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import globals from "globals";
 import { defineConfig, globalIgnores } from "eslint/config";
+import prettier from "eslint-config-prettier";
 
 export default defineConfig([
   globalIgnores(["dist", "node_modules"]),
   {
     files: ["**/*.ts"],
-    extends: [js.configs.recommended, tseslint.configs.recommended],
+    extends: [js.configs.recommended, tseslint.configs.recommended, prettier],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.node,
--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -23,7 +23,7 @@ export default defineConfig([
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": [
         "warn",
-        { argsIgnorePattern: "^_" },
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
       ],
     },
   },
